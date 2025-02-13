3938eb3972278c6276157b353437d7caf2b938d9
Devin Frazier <Dev.fraz18@gmail.com>
2025-02-12 22:32:16 -0500

    added functions, started a table but could't quite nail it

--- index.js
+++ index.js
@@ -24,13 +24,50 @@ function makeFreelancer() {
 }
 
 const freelancers = Array.from({ length: NUM_FREELANCERS }, makeFreelancer);
+
 // === Components ===
 
-function FreelancerCard(freelancer) {
+function FreelancerRow(freelancer) {
   const { freelancerName, freelancerOccupation, freelancerRate } = freelancer;
 
-  const $card = document.createElement("article");
+  const $row = document.createElement("section");
+  $row.innerHTML = `
+  <span id ="name">${freelancerName}</span>
+  <span id = "occupation">${freelancerOccupation}</span>
+  <span id = "rate">${freelancerRate}</span>
+  `;
+  return $row;
 }
+
+function FreelancerRows() {
+  const $rows = document.createElement("article");
+  const $freelancers = freelancers.map(FreelancerRow);
+  $rows.replaceChildren(...$freelancers);
+  return $rows;
+}
+
+const averageRate = () => (PRICE_RANGE.min + PRICE_RANGE.max) / 2;
+
+/*function test() {
+  const tableBody = document.querySelector("#freelancerRows");
+
+  freelancers.forEach((freelancer) => {
+    const row = tableBody.createElement("tr");
+  });
+  const nameCell = row.createElement("td");
+  nameCell.textcontent = freelancers.name;
+  row.appendChild(nameCell);
+
+  const occupationCell = row.createElement("td");
+  occupationCell.textContent = freelancers.occupation;
+  row.appendChild(occupationCell);
+
+  const rateCell = row.createElement("td");
+  rateCell.textContent = freelancers.rate;
+  row.appendChild(rateCell);
+
+  tableBody.appendChild(row);
+}*/
 
 // === Render ===
 
@@ -38,8 +75,19 @@ function render() {
   const $app = document.querySelector("#app");
   $app.innerHTML = `
   <h1>Freelancer Forum</h1>
-  <p>The average rate is $${(PRICE_RANGE.min + PRICE_RANGE.max) / 2}</p>
-  <FreelancerCards></FreelancerCards>
+  <p>The average rate is $${averageRate()}</p>
+   <table>
+   <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Occupation</th>
+        <th scope="col">Rate</th>
+      </tr>
+      </thead>
+      <tbody id="freelancerRows"></tbody>
+    </table>
+  <FreelancerRows></FreelancerRows>
   `;
+  $app.querySelector("FreelancerRows").replaceWith(FreelancerRows(freelancers));
 }
 render();
